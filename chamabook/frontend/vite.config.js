54d397986d5849cc4079719176717bd6fcf3b1ab
oketch <oketchdishon@gmail.com>
2026-04-26 17:02:54 +0300

    Conflict resolve

--- chamabook/frontend/vite.config.js
+++ chamabook/frontend/vite.config.js
@@ -9,11 +9,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       // Cache all app assets for offline use
       workbox: {
-<<<<<<< HEAD
-=======
     	skipWaiting: true,
     	clientsClaim: true,
->>>>>>> master
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
